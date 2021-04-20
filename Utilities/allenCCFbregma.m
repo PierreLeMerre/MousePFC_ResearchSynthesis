939473f9fa2aa34032a44df1aadaf32e361675d1
Pierre Le Merre <50451055+PierreLeMerre@users.noreply.github.com>
2021-04-20 12:02:09 +0200

    Update allenCCFbregma.m

--- Utilities/allenCCFbregma.m
+++ Utilities/allenCCFbregma.m
@@ -1,8 +1,8 @@
 
 function bregma = allenCCFbregma()
-% function bregma = allenCCFbregma()
+% Adapted from the Cortex lab repository : allenCCF/Browsing Functions/allenCCFbregma()
 %
-% Return coordinates of bregma in allen CCF 10um volume coordinates
-% This is just an estimate by me, the only one that's definitely right is
+% Return an estimate of the coordinates of bregma in allen CCF 10um volume coordinates
+% This is just an estimate by me (see readme.md), the only one that's definitely right is
 % the third dimension which is the midline
 bregma = [540 65 570]; % AP, DV, LR
