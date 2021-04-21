20f15cccfd512696d49110e45e176b7a7322375c
Pierre Le Merre <50451055+PierreLeMerre@users.noreply.github.com>
2021-04-21 14:47:51 +0200

    Update A_bar_graph_years.py

--- A_bar_graph_years.py
+++ A_bar_graph_years.py
@@ -11,7 +11,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 file_location = "/Path/To/Table1-Database.xlsx"
-df = pd.read_excel(file_location)
+df = pd.read_excel(file_location) # for xlsx
+# df = pd.read_csv(file_location, sep=';') # for csv
 d = df[['Year']]  #select two columns in df
 (unique, counts) = np.unique(d.Year,return_counts=True)
 
